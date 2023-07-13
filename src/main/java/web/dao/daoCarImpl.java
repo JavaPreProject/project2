27286f0f7e8178886cbb60a0ce69d0ccf12a39b9
MaximButmanov <butmanovmax@gmail.com>
2023-07-13 20:06:59 +0300

    simple commit

--- src/main/java/web/dao/daoCarImpl.java
+++ src/main/java/web/dao/daoCarImpl.java
@@ -11,8 +11,8 @@ public class daoCarImpl implements DaoCar {
     private final List<Car> listCar= new ArrayList<>(6);
     @Override
     public List<Car> getListCar() {
-        for (int i = 0; i < 5; i ++ ) {
-            listCar.add(new Car("nameCar" + i, "seriesCar" + i, i));
+        for (int i = 1; i < 6; i ++ ) {
+            listCar.add(new Car("ВАЗ 210" + i, String.valueOf((int)(Math.random()*1_000_000)), i+20));
         }
         return listCar;
     }
