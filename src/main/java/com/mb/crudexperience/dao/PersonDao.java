04106f5e7e8a0782ea8a11a5eda13711b91c1f63
MaximButmanov <butmanovmax@gmail.com>
2023-07-22 11:44:09 +0300

    initial commit

---
+++ src/main/java/com/mb/crudexperience/dao/PersonDao.java
@@ -0,0 +1,43 @@
+package com.mb.crudexperience.dao;
+
+import com.mb.crudexperience.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+    private List<Person>people;
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(PEOPLE_COUNT++, "Tom"));
+        people.add(new Person(PEOPLE_COUNT++, "Bob"));
+        people.add(new Person(PEOPLE_COUNT++, "Mike"));
+        people.add(new Person(PEOPLE_COUNT++, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
+    }
+
+    public void save (Person person) {
+        person.setId(PEOPLE_COUNT++);
+        people.add(person);
+    }
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+    public void delete (int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+
+}
