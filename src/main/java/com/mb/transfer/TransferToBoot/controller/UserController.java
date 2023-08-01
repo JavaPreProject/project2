86c08d4018e60fcac295901a0a69589f9506b527
MaximButmanov <butmanovmax@gmail.com>
2023-08-01 10:41:47 +0300

    add validation

--- src/main/java/com/mb/transfer/TransferToBoot/controller/UserController.java
+++ src/main/java/com/mb/transfer/TransferToBoot/controller/UserController.java
@@ -2,16 +2,12 @@ package com.mb.transfer.TransferToBoot.controller;
 
 import com.mb.transfer.TransferToBoot.models.User;
 import com.mb.transfer.TransferToBoot.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.ArrayList;
@@ -45,26 +41,31 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public String showUser(@PathVariable int id, Model model) {
+    public String showUser(@PathVariable("id") int id, Model model) {
         model.addAttribute("user", userService.getUserById(id));
         return "showUser";
     }
 
     @DeleteMapping("/users/{id}")
-    public String deleteUser(@PathVariable int id) {
+    public String deleteUser(@PathVariable("id") int id) {
         userService.removeUserById(id);
         return "redirect:/users";
     }
 
     @GetMapping("users/new")
-    public String newUser(@ModelAttribute User user) {
+    public String newUser(@ModelAttribute("user") User user) {
         return "new";
     }
 
     @PostMapping("users")
-    public String create(@ModelAttribute User user) {
+    public String create( @ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "new";
+        }
+
         userService.addUser(user);
         return "redirect:/users";
+        //return "users";
     }
 
     @GetMapping("users/{id}/edit")
@@ -74,7 +75,10 @@ public class UserController {
     }
 
     @PatchMapping("users/{id}")
-    public String updateUser(@ModelAttribute User user, @PathVariable int id) {
+    public String updateUser(@ModelAttribute @Valid User user, BindingResult bindingResult, @PathVariable int id) {
+        if(bindingResult.hasErrors()) {
+            return "edit";
+        }
         userService.updateUser(id, user.getName(), user.getLastName(), user.getAge());
         return "redirect:/users";
     }
--- src/main/java/com/mb/transfer/TransferToBoot/models/User.java
+++ src/main/java/com/mb/transfer/TransferToBoot/models/User.java
@@ -6,6 +6,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "users")
@@ -15,12 +20,18 @@ public class User {
     private int id;
 
     @Column(name = "name")
+    @NotBlank(message = "Имя не может быть пустым")
+    @Size(min = 2, max = 30, message = "field must contain from 2 to 30")
+    @Pattern(regexp = "^[a-zA-Zа-яА-Я]+$", message = "Имя может содержать только буквы")
     private String name;
 
     @Column(name = "last_name")
+    @NotBlank(message = "Фамилия не может быть пустой")
+    @Pattern(regexp = "^[a-zA-Zа-яА-Я]+$", message = "Фамилия может содержать только буквы")
     private String lastName;
 
     @Column(name = "age")
+    @Min(value = 0, message = "Возраст должен быть больше 0")
     private int age;
 
     public User() {
